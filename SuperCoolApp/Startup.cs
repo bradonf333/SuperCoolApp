91ccbee775f8f0f83f4bb1c7392c9e55348c86b3
bradonf333 <bradonf123@gmail.com>
2017-09-17 19:40:59 -0600

    First Commit

--- SuperCoolApp/Startup.cs
+++ SuperCoolApp/Startup.cs
@@ -17,7 +17,6 @@ namespace SuperCoolApp
         public void ConfigureServices(IServiceCollection services)
         {
         }
-        //hello
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
